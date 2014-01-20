9a09ed33bdb7d6828682eb5d85f725ea20800443
Sam Britt <samuelbritt@gmail.com>
2014-01-20 13:22:20 -0500

    add mortgage pmi

--- common_loans.py
+++ common_loans.py
@@ -1,16 +1,77 @@
 import amortized_loan
 
+class MortgagePayment(object):
+    def __init__(self, loan_payment, pmi_payment_amount):
+        super().__init__()
+
+        self.loan_payment = loan_payment
+        self.date = loan_payment.date
+        self.loan_payment_amount = loan_payment.payment_amount
+        self.principle_amount = loan_payment.principle_amount
+        self.interest_amount = loan_payment.interest_amount
+        self.new_balance = loan_payment.new_balance
+        self.pmi_amount = pmi_payment_amount
+        self.total_payment_amount = loan_payment.payment_amount + pmi_payment_amount
+
+    def __str__(self):
+        return self.__repr__()
+
+    def __repr__(self):
+        return "Payment {date} {amt:>8.2f} ({princ:>8.2f} P, {int:>8.2f} I, {pmi:>8.2f} PMI {rem:>10.2f} R)".format(
+            date=self.date.isoformat(),
+            amt=self.total_payment_amount,
+            princ=self.principle_amount,
+            int=self.interest_amount,
+            pmi=self.pmi_amount,
+            rem=self.new_balance
+        )
+
+class PMI(object):
+    """docstring for PMI"""
+    def __init__(self, mortgage, rate=0.015):
+        super().__init__()
+        self.mortgage = mortgage
+        self.rate = rate
+
+    @property
+    def standard_monthly_pmi_payment(self):
+        return self.rate * self.mortgage.financed_amount / 12
+    
+    @property
+    def pmi_payment(self):
+        if self.mortgage.current_percent_equity < 0.20:
+            return self.standard_monthly_pmi_payment
+        else:
+            return 0
+
+
 class Mortgage(object):
     def __init__(self):
         super().__init__()
         
         self.purchase_date = None
         self.purchase_amount = None
+
         self.down_payment_percent = None
         self.apr = None
         self.term_in_years = None
+        self.pmi_rate = None
 
         self._mortgage_loan = None
+        self._pmi = None
+        self._payments = []
+
+    @property
+    def current_home_value(self):
+        return self.purchase_amount
+
+    @property
+    def current_equity(self):
+        return self.current_home_value - self.mortgage_loan.remaining_balance
+
+    @property
+    def current_percent_equity(self):
+        return self.current_equity / self.current_home_value    
 
     @property
     def down_payment_amount(self):
@@ -22,7 +83,7 @@ class Mortgage(object):
 
     @property
     def mortgage_loan(self):
-        if self._mortgage_loan is None
+        if self._mortgage_loan is None:
             self._mortgage_loan = amortized_loan.AmortizedLoan(self.financed_amount,
                                                                self.term_in_years,
                                                                self.apr,
@@ -35,16 +96,47 @@ class Mortgage(object):
         return self.mortgage_loan.minimum_payment
 
     @property
+    def last_payment_date(self):
+        return self.mortgage_loan.last_payment_date
+
+    @property
+    def remaining_balance(self):
+        return self.mortgage_loan.remaining_balance
+
+    @property
     def total_interest_paid(self):
         return self.mortgage_loan.total_interest_paid
 
     @property
+    def total_pmi_paid(self):
+        if not self.payments:
+            return 0
+        return sum(p.pmi_amount for p in self.payments)
+
+    @property
+    def pmi(self):
+        if self._pmi is None:
+            self._pmi = PMI(self, self.pmi_rate)
+        return self._pmi
+    
+    @property
+    def pmi_payment(self):
+        return self.pmi.pmi_payment
+
+    @property
     def payments(self):
-        return self.mortgage_loan.payments
+        return self._payments
 
-    def make_payment(self, payment_amount=None, payment_date=None):
-        return self.mortgage_loan.make_payment(self, payment_amount, payment_date)
+    def make_loan_payment(self, payment_amount, payment_date):
+        return self.mortgage_loan.make_payment(payment_amount, payment_date)
 
+    def make_payment(self, payment_date=None):
+        pmi_payment = self.pmi.pmi_payment
+        loan_payment = self.mortgage_loan.make_payment(self.minimum_payment, payment_date)
+        mortgage_payment = MortgagePayment(loan_payment, pmi_payment)
+        self.payments.append(mortgage_payment)
 
     def calculate_amortization_table(self, regular_payment_amount=None):
-        return self.mortgage_loan.calculate_amortization_table(regular_payment_amount)
+        while self.mortgage_loan.remaining_balance > 0:
+            self.make_payment()
+        return self.payments
---
+++ test_mortgage.py
@@ -0,0 +1,130 @@
+import unittest
+import datetime
+import copy
+from pprint import pprint
+
+import common_loans
+
+# See  http://www.mlcalc.com
+
+class TestMortgageZeroDown(unittest.TestCase):
+    def setUp(self):
+        
+        self.purchase_date = datetime.date(2014,1,1)
+        self.amt = 200000
+        self.apr = 0.05
+        self.term = 30
+        self.pmi_rate = 0.01
+        self.down_payment_percent = 0.0
+
+        self.loan = common_loans.Mortgage()
+        self.loan.purchase_date = self.purchase_date
+        self.loan.purchase_amount = self.amt
+        self.loan.apr = self.apr
+        self.loan.down_payment_percent = self.down_payment_percent
+        self.loan.term_in_years = self.term
+        self.loan.pmi_rate = self.pmi_rate
+
+    def tearDown(self):
+        pass
+
+    def test_min_pmt(self):
+        self.assertAlmostEqual(self.loan.minimum_payment, 1073.64, 2)
+
+    def test_pmi_payment(self):
+        self.assertAlmostEqual(self.loan.pmi_payment, 166.67, 2)
+
+    def test_final_results(self):
+        self.assertEqual(self.loan.last_payment_date, datetime.date(2014, 1, 1))
+        self.assertAlmostEqual(self.loan.remaining_balance, self.amt)
+        self.assertAlmostEqual(self.loan.total_interest_paid, 0, 2)
+        self.assertAlmostEqual(self.loan.total_pmi_paid, 0, 2)
+
+        payments = self.loan.calculate_amortization_table()
+
+        self.assertEqual(self.loan.last_payment_date, datetime.date(2044, 1, 1))
+        self.assertAlmostEqual(self.loan.remaining_balance, 0)
+        self.assertAlmostEqual(self.loan.total_interest_paid, 186511.57, 2)
+        self.assertAlmostEqual(self.loan.total_pmi_paid, 21166.67, 2)
+
+    def test_first_payment(self):
+        payments = self.loan.calculate_amortization_table()
+        self.assertEqual(payments[0].date, datetime.date(2014, 2, 1))
+        self.assertAlmostEqual(payments[0].interest_amount, 833.33, 2)
+        self.assertAlmostEqual(payments[0].principle_amount, 240.31, 2)
+        self.assertAlmostEqual(payments[0].pmi_amount, 166.67, 2)
+
+    def test_last_payment(self):
+        payments = self.loan.calculate_amortization_table()
+        self.assertEqual(payments[-1].date, datetime.date(2044, 1, 1))
+        self.assertEqual(self.loan.last_payment_date, datetime.date(2044, 1, 1))
+        self.assertAlmostEqual(payments[-1].interest_amount, 4.45, 2)
+        self.assertAlmostEqual(payments[-1].principle_amount, 1069.19, 2)
+        self.assertAlmostEqual(payments[-1].pmi_amount, 0, 2)
+
+    def test_table(self):
+        return
+        pprint(self.loan.calculate_amortization_table())
+
+
+class TestMortgage30Down(unittest.TestCase):
+    def setUp(self):
+        self.purchase_date = datetime.date(2014,1,1)
+        self.amt = 200000
+        self.apr = 0.05
+        self.term = 30
+        self.pmi_rate = 0.01
+        self.down_payment_percent = 0.30
+
+        self.loan = common_loans.Mortgage()
+        self.loan.purchase_date = self.purchase_date
+        self.loan.purchase_amount = self.amt
+        self.loan.apr = self.apr
+        self.loan.down_payment_percent = self.down_payment_percent
+        self.loan.term_in_years = self.term
+        self.loan.pmi_rate = self.pmi_rate
+
+
+    def tearDown(self):
+        pass
+
+    def test_min_pmt(self):
+        self.assertAlmostEqual(self.loan.minimum_payment, 751.55, 2)
+
+    def test_pmi_payment(self):
+        self.assertAlmostEqual(self.loan.pmi_payment, 0, 2)
+
+    def test_final_results(self):
+        self.assertEqual(self.loan.last_payment_date, datetime.date(2014, 1, 1))
+        self.assertAlmostEqual(self.loan.remaining_balance, 140000)
+        self.assertAlmostEqual(self.loan.total_interest_paid, 0, 2)
+        self.assertAlmostEqual(self.loan.total_pmi_paid, 0, 2)
+
+        payments = self.loan.calculate_amortization_table()
+
+        self.assertEqual(self.loan.last_payment_date, datetime.date(2044, 1, 1))
+        self.assertAlmostEqual(self.loan.remaining_balance, 0)
+        self.assertAlmostEqual(self.loan.total_interest_paid, 130558.10, 2)
+        self.assertAlmostEqual(self.loan.total_pmi_paid, 0, 2)
+
+    def test_first_payment(self):
+        payments = self.loan.calculate_amortization_table()
+        self.assertEqual(payments[0].date, datetime.date(2014, 2, 1))
+        self.assertAlmostEqual(payments[0].interest_amount, 583.33, 2)
+        self.assertAlmostEqual(payments[0].principle_amount, 168.22, 2)
+        self.assertAlmostEqual(payments[0].pmi_amount, 0, 2)
+
+    def test_last_payment(self):
+        payments = self.loan.calculate_amortization_table()
+        self.assertEqual(payments[-1].date, datetime.date(2044, 1, 1))
+        self.assertEqual(self.loan.last_payment_date, datetime.date(2044, 1, 1))
+        self.assertAlmostEqual(payments[-1].interest_amount, 3.12, 2)
+        self.assertAlmostEqual(payments[-1].principle_amount, 748.43, 2)
+        self.assertAlmostEqual(payments[-1].pmi_amount, 0, 2)
+
+    def test_table(self):
+        return
+        pprint(self.loan.calculate_amortization_table())
+
+if __name__ == '__main__':
+    unittest.main()
